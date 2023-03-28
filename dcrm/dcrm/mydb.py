a9aac58987af93e4c4e562e83eb977affadd4720
Austin Ward <austsethw@hotmail.com>
2023-03-27 23:49:36 -0400

    registration

--- dcrm/dcrm/mydb.py
+++ dcrm/dcrm/mydb.py
@@ -1,3 +1,7 @@
+# This code is just for creating a database. Once it is
+# run then it is no longer needed and can be deleted. It 
+# remains here for demonstration purposes. -Austin Ward
+
 import mysql.connector
 
 dataBase = mysql.connector.connect(
